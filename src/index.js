40743df125c2f0e8b6a4a1ac42169caf5835f6f5
Milhan Osman <milhanomarosman@gmail.com>
2023-03-24 09:18:02 +0300

    completed assigment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
+const createTaskForm = document.getElementById('create-task-form');
+const input = document.getElementById('new-task-description');
+const btn = document.getElementsByClassName('btn');
+
+
+createTaskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
   // your code here
+  let li = document.createElement('li');
+  li.innerHTML = `<p>${input.value} <button class='btn'> Delete </button></p>`;
+  document.getElementById("tasks").append(li)
+  input.value = "";
 });
+
+btn.addEventListener('click', () => {
+  let parent = btn.parentElement;
+  parent.remove();
+})
