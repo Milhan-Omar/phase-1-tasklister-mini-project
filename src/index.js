e8d4c5477dafc7e7af80831edf0f1f3f89fabaa4
Milhan Osman <milhanomarosman@gmail.com>
2023-03-24 19:25:11 +0300

    completed assignent

--- src/index.js
+++ src/index.js
@@ -6,13 +6,49 @@ const btn = document.getElementsByClassName('btn');
 createTaskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   // your code here
-  let li = document.createElement('li');
-  li.innerHTML = `<p>${input.value} <button class='btn'> Delete </button></p>`;
-  document.getElementById("tasks").append(li)
+  buildList(input.value);
   input.value = "";
 });
 
-btn.addEventListener('click', () => {
-  let parent = btn.parentElement;
-  parent.remove();
-})
+// btn.addEventListener('click', () => {
+//   let parent = btn.parentElement;
+//   parent.delete();
+// })
+// function sortList(e){
+//  e. 
+// }
+// document.addEventListener("DOMContentLoaded", () => {
+  // your code here
+  // const form = document.getElementById('create-task-form');
+  // form.addEventListener('su', (e) => {
+  //   e.preventDefault()
+  //   buildList(e.target.new_task.value)
+  //   // Reset Form Here!!
+  //   form.reset()
+  // });
+// })
+// Function to build out To-DO
+
+const buildList = (todo) => {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.style.backgroundColor = 'red'
+  btn.addEventListener('click', deleteHandler)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+function deleteHandler(e) {
+  e.target.parentNode.remove()
+}
+function sortList(e) {
+  e.target.parentNode.sort()
+} 
+
+
+
+
+
+
